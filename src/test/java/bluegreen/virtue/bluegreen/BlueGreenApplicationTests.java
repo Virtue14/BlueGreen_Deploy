3f75fbc80a562e9cf3d35731bd6b8c55e3ebfabf
악덕 <rudejrqkr97@gmail.com>
2024-07-12 11:34:21 +0900

    :tada: init

---
+++ src/test/java/bluegreen/virtue/bluegreen/BlueGreenApplicationTests.java
@@ -0,0 +1,13 @@
+package bluegreen.virtue.bluegreen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlueGreenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
